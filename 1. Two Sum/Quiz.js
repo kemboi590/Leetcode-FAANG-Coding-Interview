f58ba0a281a69ac9439a163f2f4ed8376515c7e9
kemboi590 <bkemboi590@gmail.com>
2024-03-06 18:27:38 +0300

    readme added

---
+++ 1. Two Sum/Quiz.js
@@ -0,0 +1,9 @@
+/**
+//LEETCODE
+ https://leetcode.com/problems/two-sum/
+
+ //VIDEO
+ https://www.youtube.com/watch?v=luicuNOBTAI&t=381s
+
+ */
+
---
+++ 2. Add Two Numbers/quiz.js
@@ -0,0 +1,5 @@
+/**
+ //LEETCODE
+
+ https://leetcode.com/problems/add-two-numbers/description/
+ */
